4f235398f734dbd4672b298eb5b08b414b1d533d
Rayan Syed <rsyed@scc1.bu.edu>
2024-07-10 17:52:15 -0400

    cache

--- scratch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-timestamp=$(date +"%Y%m%d%H%M%S%N")  # %N for nanoseconds
-
-random_suffix=$(($RANDOM % 1000))  # Generates a random number between 0 and 999
-
-jobname="scratch_dataloading_${timestamp}_${random_suffix}"
-
-qsub -N "${jobname}" -o "/projectnb/tianlabdl/rsyed/scratch_dataloading/logs/${jobname}.qlog" "scratch.qsub"
