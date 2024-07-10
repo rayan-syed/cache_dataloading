3168ae8771ac2ced5177703b8cafb1fab8eb19c7
Rayan Syed <rsyed@bu.edu>
2024-07-10 17:41:39 -0400

    dummy data generator

---
+++ data/data.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+from PIL import Image
+
+data_path = './dummy_data'
+os.makedirs(data_path, exist_ok=True)
+
+# Generate 5000 random PNGS
+for i in range(5000):
+    # Generate random pixel values for each channel (RGB)
+    random_pixels = np.random.randint(0,256, (28, 28, 3), dtype=np.uint8)
+    # Create an image from the random pixel values
+    image = Image.fromarray(random_pixels, mode='RGB')
+    # Save new image
+    image.save(os.path.join(data_path, f'image_{i}.png'))
+
+print("Dummy data generation complete")
+
