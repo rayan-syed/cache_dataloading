69b0f60584c7e1d93024750085b87694107cab15
Rayan Syed <rsyed@scc1.bu.edu>
2024-07-10 19:06:42 -0400

    Traverse Layers

--- cache.py
+++ cache.py
@@ -8,11 +8,13 @@ from torchvision import transforms
 from PIL import Image
 import time
 
+workers = 4
+
 projectnb = f'./data/dummy_data'   # projectnb
 engnas= f'/ad/eng/research/eng_research_cisl/rsyed/dummy_data' # engnas
 
 # Change these paths accordingly 
-data_path = engnas
+data_path = projectnb
 cache_path = f'/scratch/rsyed/data'
 
 class DataCache:
@@ -68,7 +70,6 @@ transform = transforms.Compose([
 dataset = CustomImageDataset(files, transform=transform)
 
 # Create DataLoader
-workers = 4             # DEFINE NUM_WORKERS HERE
 train_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=workers) 
 
 # Model With Customizable Layers
@@ -93,28 +94,29 @@ class SimpleCNN(nn.Module):
         x = x.view(x.size(0), -1)  
         x = self.fc1(x)
         return x
+    
+test_layers = (20,30,40,50,60)
+for layers in test_layers:
+    model = SimpleCNN(num_conv_layers=layers)                     
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-layers = 20             # DEFINE NUM_LAYERS HERE
-model = SimpleCNN(num_conv_layers=layers)                     
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # Training loop
+    epochs = 5
+    start = time.time()  # Start timer
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        for images in train_loader:
+            optimizer.zero_grad()
+            outputs = model(images)
+            labels = torch.randint(0, 10, (images.size(0),)) # Fake labels
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print(f"Epoch {epoch+1}/{epochs}")
+    end = time.time()   # End timer
+    timer = end-start    # Calculate time
 
-# Training loop
-epochs = 3
-start = time.time()  # Start timer
-for epoch in range(epochs):
-    model.train()
-    running_loss = 0.0
-    for images in train_loader:
-        optimizer.zero_grad()
-        outputs = model(images)
-        labels = torch.randint(0, 10, (images.size(0),)) # Fake labels
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-    print(f"Epoch {epoch+1}/{epochs}")
-end = time.time()   # End timer
-time = end-start    # Calculate time
-
-print(f"Training with {layers} layers and {workers} workers finished in Elapsed time: {time:.2f} seconds")
+    print(f"Training finished with Layers: {layers} in Time: {timer:.2f} seconds")
